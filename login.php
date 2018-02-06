2744c8bb219eb316e70c5061c12df36181d07291
Mayank Mehta <32803024+mayankm276@users.noreply.github.com>
2018-02-07 01:40:51 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
+<form method="post">
+	<div><input type="text" name="username" placeholder="Username"></div>
+	<div><input type="password" name="password" placeholder="Password"></div>
+	<div><input class="sub" type="submit" value="Login"></div><br>
+	<div><a href="register.php">Register</a></div>
+</form>
+</body>
+</html>
