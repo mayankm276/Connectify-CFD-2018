fb324462159338fc3819d4d288d1f29fa20b34cb
Mayank Mehta <32803024+mayankm276@users.noreply.github.com>
2018-02-07 21:08:05 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -1,18 +1,76 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>login</title>
+	<title></title>
 	<meta charset="utf-8">
   	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="login.css">
 </head>
 <body>
-<form method="post">
-	<div class="logo"><img src="images/logo_white.png" height="100px"></div>
-	<div><input type="text" name="username" placeholder="Username"></div>
-	<div><input type="password" name="password" placeholder="Password"></div>
-	<div><input class="sub" type="submit" value="Login"></div><br>
-	<div><a href="register.php">Register</a></div>
-</form>
+	<a href="" id="home">HOME</a>
+<div class="boxed log" id="login">
+	<form method="post" action="">
+		<div class="logo"><img src="images/logo_white.png" height="100px"></div>
+		<div><input type="text" name="username" placeholder="Username"></div>
+		<div><input type="password" name="password" placeholder="Password"></div>
+		<div><input class="sub" type="submit" value="Login"></div>
+		<div><input class="sub" type="button" value="Register Individual" onclick="myFunctionRegister()"></input></div>
+		<div><input class="sub" type="button" value="Create Group" onclick="myFunctionRegisterTeam()"></input></div>
+	</form>
+</div>
+<div class="boxed reg" id="register">
+	<form method="post" action="">
+		<div class="logo"><img src="images/logo_white.png" height="100px"></div>
+		<div><input type="text" name="fname" placeholder="First name"></div>
+		<div><input type="text" name="lname" placeholder="Last name"></div>
+		<div><input type="text" name="username" placeholder="Username"></div>
+		<div><input type="text" name="email" placeholder="Email id"></div>
+		<div><input type="password" name="pwd1" placeholder="Enter Password"></div>
+		<div><input type="password" name="pwd2" placeholder="Re-enter Password"></div>
+		<div><input class="sub" type="submit" value="Create a new account"></div>
+		<div><input class="sub" type="button" value="Back to Login" onclick="myFunctionLogin()"></input></div>
+		<div><input class="sub" type="button" value="Create Group" onclick="myFunctionRegisterTeam()"></input></div>
+	</form>
+</div>
+<div class="boxed reg" id="registerTeam">
+	<form method="post" action="">
+		<div class="logo"><img src="images/logo_white.png" height="100px"></div>
+		<div><input type="text" name="gname" placeholder="Group name"></div>
+		<div><input type="text" name="username1" placeholder="Username"></div>
+		<div><input type="text" name="username2" placeholder="Username"></div>
+		<div><input type="text" name="username3" placeholder="Username"></div>
+		<div><input type="password" name="pwd1" placeholder="Enter Password"></div>
+		<div><input type="password" name="pwd2" placeholder="Re-enter Password"></div>
+		<div><input class="sub" type="submit" value="Create a new Group"></div>
+		<div><input class="sub" type="button" value="Register Individual" onclick="myFunctionRegister()"></input></div>
+		<div><input class="sub" type="button" value="Back to Login" onclick="myFunctionLogin()"></input></div>
+	</form>
+</div>
+<script>
+function myFunctionRegister() {
+    document.getElementById("register").style.zIndex = "1";
+    document.getElementById("register").style.display = "block";
+    document.getElementById("registerTeam").style.zIndex = "-1";
+    document.getElementById("registerTeam").style.display = "none";
+    document.getElementById("login").style.zIndex = "-1";
+    document.getElementById("login").style.display = "none";
+}
+function myFunctionRegisterTeam() {
+    document.getElementById("register").style.zIndex = "-1";
+    document.getElementById("register").style.display = "none";
+    document.getElementById("registerTeam").style.zIndex = "1";
+    document.getElementById("registerTeam").style.display = "block";
+    document.getElementById("login").style.zIndex = "-1";
+    document.getElementById("login").style.display = "none";
+}
+function myFunctionLogin() {
+    document.getElementById("register").style.zIndex = "-1";
+    document.getElementById("register").style.display = "none";
+    document.getElementById("registerTeam").style.zIndex = "-1";
+    document.getElementById("registerTeam").style.display = "none";
+    document.getElementById("login").style.zIndex = "1";
+    document.getElementById("login").style.display = "block";
+}
+</script>
 </body>
 </html>
