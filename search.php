e2ec9986e8d03675399f4736ff6c25432b0960ba
Mayank Mehta <32803024+mayankm276@users.noreply.github.com>
2018-02-07 21:12:55 +0530

    Add files via upload

---
+++ search.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="search.css">
+</head>
+<body>
+	<div class="bar">
+		<div class="button"><input id="skills" type="button" value="Search via skills"></input></div>
+		<div class="button"><input id="person" type="button" value="Search for person"></input></div>
+	</div>
+	<div class="logo"><img src="images/logo_white.png" height="200px"></div>
+	<div class="container">
+		<form method="get" action="">
+			<input class="search" type="text" name="search" placeholder="Search...">
+			<input class="submit" type="submit">
+		</form>
+	</div>
+</body>
+</html>
